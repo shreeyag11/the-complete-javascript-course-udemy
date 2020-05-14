60f75391d80f55350ddb197ea28ca047a526b346
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-14 17:40:42 -0400

    Calculate BMI of Mark and John

---
+++ CodingChallenge1/script.js
@@ -0,0 +1,12 @@
+console.log("I am Working");
+
+var markHeight, markWeight, johnHeight, johnWeight;
+markHeight = prompt("Enter Mark's height");
+markWeight = prompt("Enter Mark's weight");
+johnHeight = prompt("Enter John's height");
+johnWeight = prompt("Enter John's weight");
+
+var markBMI = markWeight / (markHeight * markHeight);
+var johnBMI = johnWeight / (johnHeight * johnHeight);
+var higher = markBMI > johnBMI;
+console.log("Is Mark's BMI higher than John's? " + higher);
